e319a7f5a79bc34252209256da63e3bdfc537cf5
migitera.yusuke <migitera.yusuke@donuts.ne.jp>
2022-03-31 12:28:48 +0900

    add: docker-compose settings

---
+++ mysql/init/1_create.sql
@@ -0,0 +1,3 @@
+-- CREATE DATABASE weather;
+USE weather;
+INSERT INTO weathers (dt, humidity, wind_speed, temp_average, temp_max, temp_min, weather, weather_icon) VALUES (1648598400, 70, 3.38, 286.96, 292.54, 282.81, "Clear", "01n");
